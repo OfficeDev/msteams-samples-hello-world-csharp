5e2f257d9a0faab91db7b1f99fe10cec54a57799
Charles Prakash Dasari <prakashd@konube.com>
2018-01-12 21:21:58 -0800

    Bot and message extension done

--- Microsoft.Teams.Samples.HelloWorld.Web/App_Start/WebApiConfig.cs
+++ Microsoft.Teams.Samples.HelloWorld.Web/App_Start/WebApiConfig.cs
@@ -1,7 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web.Http;
+﻿using System.Web.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace Microsoft.Teams.Samples.HelloWorld.Web
 {
@@ -9,6 +8,17 @@ namespace Microsoft.Teams.Samples.HelloWorld.Web
     {
         public static void Register(HttpConfiguration config)
         {
+            // Json settings
+            config.Formatters.JsonFormatter.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            config.Formatters.JsonFormatter.SerializerSettings.Formatting = Formatting.Indented;
+            JsonConvert.DefaultSettings = () => new JsonSerializerSettings()
+            {
+                ContractResolver = new CamelCasePropertyNamesContractResolver(),
+                Formatting = Formatting.Indented,
+                NullValueHandling = NullValueHandling.Ignore,
+            };
+
             // Web API configuration and services
 
             // Web API routes
