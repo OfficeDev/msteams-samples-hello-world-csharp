91a32886a135ba0f8a8af29a2252494597215282
Aditya Lele <adlele@microsoft.com>
2020-04-08 11:47:54 -0700

    initial commit - migrated to dotnet core with bot framework v4

--- Microsoft.Teams.Samples.HelloWorld.Web/Controllers/HomeController.cs
+++ Microsoft.Teams.Samples.HelloWorld.Web/Controllers/HomeController.cs
@@ -1,4 +1,8 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Microsoft.Teams.Samples.HelloWorld.Web.Controllers
 {
--- Microsoft.Teams.Samples.HelloWorld.Web/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Microsoft.Teams.Samples.HelloWorld.Web.MvcApplication" Language="C#" %>
